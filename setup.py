7bddfb005e1fe060735abc7401c03b506d63edec
Byron <37745048+byhowe@users.noreply.github.com>
2020-03-17 13:46:58 -0500

    Added setup.py for easy installation

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name="finian",
+    version="0.1",
+    packages=find_packages(),
+
+    install_requires=[
+        "rsa"
+    ],
+
+    author=""Byron"",
+    author_email="37745048+byhowe@users.noreply.github.com",
+    description="A library that utilizes RSA encryption to make sockets secure.",
+    url="https://github.com/byhowe/python-networking"
+)
